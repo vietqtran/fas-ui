5d9d6ea029880d4be7194f55ea49fdf81a7bca59
vietqtran <tranquocviet1303@gmail.com>
2024-01-22 21:37:42 +0700

    ✨feature: demo SOC

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const AdminPage = (props: Props) => {
+   return <div>AdminPage</div>
+}
+
+export default AdminPage
---
+++ components/Login/LoginInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const LoginInput = (props: Props) => {
+   return <div>LoginInput</div>
+}
+
+export default LoginInput
