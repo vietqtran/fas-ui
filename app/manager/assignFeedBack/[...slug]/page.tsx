f02b5bad9eceec7e72a2547b757e6b45dfc07dc6
LowkyVu <cuongmc1404@gmail.com>
2024-02-26 10:43:26 +0700

    update code

--- app/manager/assignFeedBack/[...slug]/page.tsx
+++ app/manager/assignFeedBack/[...slug]/page.tsx
@@ -10,7 +10,7 @@ import {
   GridValueGetterParams,
 } from "@mui/x-data-grid";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import React, { use, useEffect } from "react";
+import React, { use, useEffect, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -19,6 +19,9 @@ import ModalStudent from "@/components/Common/Modals/ModalStudent";
 import ModalAddStudentToGrade from "@/components/Common/Modals/ModalAddStudentToGrade";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useRouter } from "next/navigation";
+import useAssignFeedBack from "@/hooks/AssignFeedBack";
+import ModalFeedback from "@/components/Common/Modals/ModalFeedback";
+import useFeedBack from "@/hooks/FeedBack";
 type Props = {
   params: {
     slug: string;
@@ -29,15 +32,29 @@ const page = (props: Props) => {
   const { slug } = props.params;
   const { getGrade, handleDeleteStudentToGrade, grade, setGrade } = useGrade();
   const [reload, setReload] = React.useState(false);
-  const { students, fetchStudentByMajorAndCampus } = useStudent();
-  const [studentArray, setStudentArray] = React.useState([]);
+
+  const [feedbackArray, setFeedbackArray] = React.useState([]);
+  const { getAssignFeedBack } = useAssignFeedBack();
+  const { deleteFeedBack } = useFeedBack();
+  const [title, setTitle] = React.useState("");
+  const [feedback, setFeedBack] = useState({
+    id: "",
+    createAt: "",
+    endDate: "",
+    grade: { code: "", id: "" },
+    instructor: {
+      id: "",
+      username: "",
+    },
+    startDate: "",
+    status: true,
+    updateAt: "",
+  });
   const getData = async (id: string) => {
-    const data = await getGrade(id);
-    const data2 = await fetchStudentByMajorAndCampus(
-      data?.major?.id,
-      data?.campus?.id
-    );
-    setStudentArray(data2);
+    const data = await getAssignFeedBack(id);
+    console.log(data);
+    setFeedbackArray(data.feedBacks);
+    setTitle(`Class ${data?.grade?.code} - ${data?.instructor?.username}`);
     setGrade(data);
     setId(slug[0]);
   };
@@ -49,7 +66,6 @@ const page = (props: Props) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
     setOpen(true);
-
     setAction("create");
   };
   const handleClose = () => {
@@ -61,89 +77,60 @@ const page = (props: Props) => {
 
   const [id, setId] = React.useState("");
   const [action, setAction] = React.useState("");
-  const [openView, setOpenView] = React.useState(false);
-  const handleView = async (idStudent: string) => {
+  const handleView = async (data) => {
+    setFeedBack(data);
     setAction("view");
-    setId(idStudent);
-    setOpenView(true);
-  };
-
-  const handleCloseView = () => {
-    setOpenView(false);
+    setOpen(true);
   };
 
   const handleDelete = (studentId: string) => {
-    handleDeleteStudentToGrade(studentId, slug[0]);
+    deleteFeedBack(studentId);
     setReload(!reload);
   };
 
-  const filteredStudents = studentArray?.filter((student) => {
-    return !grade?.students.some((grade) => grade.id === student.id);
-  });
-
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
+    { field: "id", headerName: "ID", width: 170 },
     {
-      field: "studentCode",
-      headerName: "Code",
-      width: 80,
+      field: "student",
+      headerName: "Student",
+      width: 240,
       renderCell: (params) => (
         <div color="primary" onClick={() => handleView(params.row.id)}>
-          {params.row.studentCode}
+          {params.row.student.username}
         </div>
       ),
     },
-    { field: "email", headerName: "Email", width: 190 },
-    { field: "firstName", headerName: "First Name", width: 100 },
-    { field: "middleName", headerName: "Middle Name", width: 110 },
-    { field: "lastName", headerName: "Last Name", width: 100 },
     {
-      field: "birthDay",
-      headerName: "Birth Day",
-      type: "dateTime",
-      width: 100,
-      valueFormatter(params) {
-        return new Date(params.value).toLocaleDateString();
-      },
+      field: "createdAt",
+      headerName: "Created At",
+      width: 310,
     },
     {
-      field: "phone",
-      headerName: "Phone",
-      width: 110,
+      field: "updatedAt",
+      headerName: "Updated At",
+      width: 310,
     },
     {
       field: "status",
       headerName: "Status",
-      width: 80,
+      width: 180,
       renderCell: (params) => (
         <div style={{ color: params.row.status ? "green" : "red" }}>
           {params.row.status ? "Active" : "Inactive"}
         </div>
       ),
     },
-    { field: "address", headerName: "Address", width: 320 },
-    {
-      field: "major",
-      headerName: "Major",
-      width: 60,
-      valueGetter: (params: GridValueGetterParams) => params.row.major?.code,
-    },
-    {
-      field: "campus",
-      headerName: "Campus",
-      width: 100,
-      valueGetter: (params: GridValueGetterParams) => params.row.campus?.name,
-    },
+
     {
       field: "action",
       headerName: "Action",
-      width: 110,
+      width: 310,
       disableColumnMenu: true,
       renderCell: (params) => (
         <div className="flex gap-3">
           <button
             className="rounded bg-blue-500 px-2 py-2 font-bold text-white hover:bg-blue-600"
-            onClick={() => handleView(params.row.id)}
+            onClick={() => handleView(params.row)}
           >
             <VisibilityIcon />
           </button>
@@ -171,27 +158,17 @@ const page = (props: Props) => {
       <div className="container">
         <h1
           className="text-3xl font-bold my-5 hover:cursor-pointer"
-          onClick={() => router.push("/manager/grades")}
+          onClick={() => router.push("/manager/assignFeedBack")}
         >
           {" "}
-          <ArrowBackIcon /> List Grade
+          <ArrowBackIcon /> List FeedBack
         </h1>
         <div className="flex justify-between items-center my-7">
-          <h1 className="text-3xl font-bold italic text-slate-600">
-            Class {grade?.code}
-          </h1>
-          <button
-            className="flex items-center gap-2 rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-            onClick={() => handleOpen()}
-          >
-            <AddIcon />
-            Add Student to Class
-          </button>
+          <h1 className="text-3xl font-bold italic text-slate-600">{title}</h1>
         </div>
-
         <div style={{ height: "100%", width: "100%" }}>
           <DataGrid
-            rows={filteredStudents}
+            rows={feedbackArray}
             columns={columns}
             initialState={{
               pagination: {
@@ -210,16 +187,10 @@ const page = (props: Props) => {
               toolbar: CustomToolbar,
             }}
           />
-          <ModalAddStudentToGrade
+          <ModalFeedback
             open={open}
             handleClose={handleClose}
-            id={id}
-            action={action}
-            studentInGrade={filteredStudents}
-          />
-          <ModalStudent
-            open={openView}
-            handleClose={handleCloseView}
+            feedback={feedback}
             id={id}
             action={action}
           />
--- helpers/api/feedback.ts
+++ helpers/api/feedback.ts
@@ -5,6 +5,7 @@ const END_POINT = {
   CREATE_FEEDBACK: "feedback",
   GET_FEEDBACK_BY_ID: "feedback",
   UPDATE_FEEDBACK: "feedback/update",
+  DELETE_FEEDBACK: "feedback/delete",
 };
 
 export const getAllFeedBack = () => {
@@ -19,6 +20,17 @@ export const addFeedBack = (feedBackReq: FeedBackInformation) => {
   return instance.post(`/${END_POINT.CREATE_FEEDBACK}`, feedBackReq);
 };
 
-export const updateFeedBack = (id: string, feedBackReq: FeedBackInformation) => {
+export const updateFeedBack = (
+  id: string,
+  feedBackReq: FeedBackInformation
+) => {
   return instance.put(`/${END_POINT.UPDATE_FEEDBACK}/${id}`, feedBackReq);
+};
+
+export const checkFeedBackExist = (assignId: string, studentId: string) => {
+  return instance.get(`/feedback/assign/${assignId}/student/${studentId}`);
+};
+
+export const deleteFeedBackById = (id: string) => {
+  return instance.put(`/${END_POINT.DELETE_FEEDBACK}/${id}`);
 };
